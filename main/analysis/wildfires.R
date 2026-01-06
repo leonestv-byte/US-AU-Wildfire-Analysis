c7825d274d3247ff6613781e110ab9c080001063
Steven Leone <darklizard979@Stevens-MacBook-Pro.local>
2026-01-06 12:32:41 -0500

    Spatial Hierarchical Model For Wildfire Data

--- main/analysis/wildfires.R
+++ main/analysis/wildfires.R
@@ -106,4 +106,63 @@ lines(p_values, likelihood_density, col = "green", lwd = 2)
 ##### Night Time - Spatial Hierarchical Models   #############
 ##############################################################
 
+library(brms)
 
+set.seed(321)
+
+data_clean <- na.omit(us_data[, c("nighttime", "brightness",
+                               "latitude", "longitude")])
+
+# 1. Scaled/centered columns
+lat_center <- mean(data_clean$latitude)
+lat_scale  <- sd(data_clean$latitude)
+lon_center <- mean(data_clean$longitude)
+lon_scale  <- sd(data_clean$longitude)
+
+# 2. Turn columns into Vectors
+data_clean$lat_sc <- as.vector((data_clean$latitude - lat_center) / lat_scale)
+data_clean$lon_sc <- as.vector((data_clean$longitude - lon_center) / lon_scale)
+
+# 3. Use geolocation to create knots
+knots <- data_clean[sample(nrow(data_clean), 300), c("lat_sc", "lon_sc")]
+
+knots <- data.frame(
+  lat_sc = as.numeric(knots$lat_sc),
+  lon_sc = as.numeric(knots$lon_sc)
+)
+
+
+fit <- brm(
+  nighttime ~ brightness +
+    s(latitude, longitude, k = 10),
+  data = data_clean,
+  family = bernoulli(),
+  chains = 1,
+  iter = 50,
+  cores = 1
+)
+
+
+summary(fit)
+# 1 chain, 50 iterations, 1 core:
+# Family: bernoulli 
+# Links: mu = logit 
+# Formula: nighttime ~ brightness + s(latitude, longitude, k = 10) 
+# Data: data_clean (Number of observations: 17532) 
+# Draws: 1 chains, each with iter = 50; warmup = 25; thin = 1;
+# total post-warmup draws = 25
+
+# Smoothing Spline Hyperparameters:
+#  Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
+# sds(slatitudelongitude_1)     0.40      0.21     0.15     0.83 1.13       10       10
+
+# Regression Coefficients:
+#   Estimate Est.Error l-95% CI u-95% CI Rhat Bulk_ESS Tail_ESS
+# Intercept               16.93      2.98    11.21    20.22 1.97        2       10
+# brightness              -0.06      0.01    -0.07    -0.04 1.97        2       10
+# slatitudelongitude_1     0.87      1.69    -1.06     3.72 1.47        3       10
+# slatitudelongitude_2    -1.57      0.76    -2.65    -0.67 2.02        2       11
+
+# Draws were sampled using sampling(NUTS). For each parameter, Bulk_ESS
+# and Tail_ESS are effective sample size measures, and Rhat is the potential
+# scale reduction factor on split chains (at convergence, Rhat = 1).
